ef560b18b3b7fe1a3341d764085e10ced53b669f
Simon Hinterseer <simon.hinterseer@tuwien.ac.at>
2024-12-06 10:47:34 +0100

    day03 done

--- 03.py
+++ 03.py
@@ -38,27 +38,21 @@ def part1_script(input_file):
 def part2_script(input_file):
     input_string = get_input(input_file)
     input_string = 'do()' + input_string
-    # find 'do()' and set postition 1
-    # try to find "don't()" and set postion 2 if not found position 2 = -1
-    # process input_string[p1:p2]
-    # repeat for string[p2:]
     current_string = input_string
     result = 0
-    i = 0
     while len(current_string) > 1:
-        p1 = current_string.find('do()')
-        p2 = p1 + current_string[p1:].find("don't()")
-        result += process_string(current_string[p1:p2])
+        current_string = current_string[current_string.find('do()'):]
+        p2 = current_string.find("don't()")
+        result += process_string(current_string[:p2])
         current_string = current_string[p2:]
     return result
 
 
 if __name__ == '__main__':
     unittest.main(exit=False)
-    #
-    # filename_global = '03_testinput.txt'
+
     filename_global = '03_input.txt'
     result1 = part1_script(filename_global)
     print(f'result 1: {result1}')
-    # result2 = part2_script(filename_global)
-    # print(f'result 2: {result2}')
+    result2 = part2_script(filename_global)
+    print(f'result 2: {result2}')
