da29ea87fbb7eb05d2908f6f440c43671d46f75a
Simon Hinterseer <simon.hinterseer@tuwien.ac.at>
2024-12-06 12:58:23 +0100

    day 04 done

--- 04.py
+++ 04.py
@@ -1,73 +1,72 @@
 import unittest
 import numpy as np
 
-# class TestExample(unittest.TestCase):
-#     filename1 = '03_testinput1.txt'
-#     filename2 = '03_testinput2.txt'
-#
-#     def test_part1(self):
-#         self.assertEqual(161, part1_script(self.filename1))
-#
-#     def test_part2(self):
-#         self.assertEqual(48, part2_script(self.filename2))
+
+class TestExample(unittest.TestCase):
+    filename = '04_testinput.txt'
+
+    def test_part1(self):
+        self.assertEqual(18, part1_script(self.filename))
+
+    def test_part2(self):
+        self.assertEqual(9, part2_script(self.filename))
 
 
 def get_input(filename):
     with open(filename) as file:
         lines = [list(line.rstrip()) for line in file]
-    dim1 = len(lines[0])
-    dim2 = len(lines)
     ar = np.array(lines)
-    # dia1 = list()
-    # for i in range(dim1):
-    #     dia1.append([lines[dim1 - i][] ])
-    # tester = [[11, 12, 13],
-    #           [21, 22, 23],
-    #           [31, 32, 33]]
-    # tarray = np.array(tester)
-    # tarrayf = np.fliplr(tarray)
-    # tarrayt = np.transpose(tarray)
     return ar
 
 
 def part1_script(input_file):
-    # count 'XMAS' in lines columns and diagonals forward and backward
+    def count_occurance(charlist, str1='XMAS', str2='SAMX'):
+        count = 0
+        count += ''.join(charlist).count(str1)
+        count += ''.join(charlist).count(str2)
+        return count
+
     chars = get_input(input_file)
+    charsf = np.fliplr(chars)
     num_rows = chars.shape[0]
     num_cols = chars.shape[1]
     result = 0
     for i in range(num_rows):
-        result += ''.join(chars[i, :]).count("XMAS")
-        result += ''.join(chars[i, :]).count("SAMX")
+        result += count_occurance(chars[i, :])
     for i in range(num_cols):
-        result += ''.join(chars[:, i]).count("XMAS")
-        result += ''.join(chars[:, i]).count("SAMX")
+        result += count_occurance(chars[:, i])
 
+    result += count_occurance(np.diagonal(chars))
+    for i in range(1, num_rows):  # diagonals
+        result += count_occurance(np.diagonal(chars, offset=i))
+        result += count_occurance(np.diagonal(chars, offset=-i))
 
-
-    x=0
+    result += count_occurance(np.diagonal(charsf))
+    for i in range(1, num_rows):  # diagonals
+        result += count_occurance(np.diagonal(charsf, offset=i))
+        result += count_occurance(np.diagonal(charsf, offset=-i))
     return result
 
 
-# def part2_script(input_file):
-#     input_string = get_input(input_file)
-#     input_string = 'do()' + input_string
-#
-#     current_string = input_string
-#     result = 0
-#     while len(current_string) > 1:
-#         current_string = current_string[current_string.find('do()'):]
-#         p2 = current_string.find("don't()")
-#         result += process_string(current_string[:p2])
-#         current_string = current_string[p2:]
-#     return result
+def part2_script(input_file):
+    chars = get_input(input_file)
+    result = 0
+    for i in range(1, chars.shape[0] - 1):
+        for j in range(1, chars.shape[1] - 1):
+            if chars[i, j] == 'A':
+                if ((chars[i - 1, j - 1] == 'M' and chars[i + 1, j + 1] == 'S') or
+                        (chars[i - 1, j - 1] == 'S' and chars[i + 1, j + 1] == 'M')):
+                    if ((chars[i + 1, j - 1] == 'M' and chars[i - 1, j + 1] == 'S') or
+                            (chars[i + 1, j - 1] == 'S' and chars[i - 1, j + 1] == 'M')):
+                        result += 1
+    return result
 
 
 if __name__ == '__main__':
     unittest.main(exit=False)
 
-    filename_global = '04_testinput.txt'
+    filename_global = '04_input.txt'
     result1 = part1_script(filename_global)
     print(f'result 1: {result1}')
-    # result2 = part2_script(filename_global)
-    # print(f'result 2: {result2}')
+    result2 = part2_script(filename_global)
+    print(f'result 2: {result2}')
