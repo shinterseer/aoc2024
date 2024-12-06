a4d72fb3808b7f95c39424d0d735efdce02e5c6d
Simon Hinterseer <simon.hinterseer@tuwien.ac.at>
2024-12-06 15:19:16 +0100

    day 05 done

--- 05.py
+++ 05.py
@@ -1,17 +1,14 @@
 import unittest
 
 
-# import numpy as np
-
-
 class TestExample(unittest.TestCase):
     filename = '05_testinput.txt'
 
     def test_part1(self):
         self.assertEqual(143, part1_script(self.filename))
 
-    # def test_part2(self):
-    #     self.assertEqual(9, part2_script(self.filename))
+    def test_part2(self):
+        self.assertEqual(123, part2_script(self.filename))
 
 
 def get_input(filename):
@@ -20,43 +17,53 @@ def get_input(filename):
 
     rules = list()
     updates = list()
-    part2 = False
+    current_list = rules
     for line in lines:
         if line == '':
-            part2 = True
+            current_list = updates
             continue
-        if part2:
-            updates.append(line)
-        else:
-            rules.append(line)
+        current_list.append(line)
 
     rules = [[int(item) for item in rule.split('|')] for rule in rules]
     updates = [[int(item) for item in update.split(',')] for update in updates]
     return rules, updates
 
 
+def update_correct(update, rules):
+    correct = True
+    for rule in rules:
+        if rule[0] in update and rule[1] in update:
+            if update.index(rule[0]) > update.index(rule[1]):
+                correct = False
+                break
+    return correct
+
+
 def part1_script(input_file):
     rules, updates = get_input(input_file)
-    correct_updates = list()
-
-    for update in updates:
-        is_ok = True
-        for rule in rules:
-            if rule[0] in update and rule[1] in update:
-                if update.index(rule[0]) > update.index(rule[1]):
-                    is_ok = False
-        if is_ok:
-            correct_updates.append(update)
-
-    result = 0
-    for update in correct_updates:
-        result += update[int(len(update) / 2)]
-
+    correct_updates = [update for update in updates if update_correct(update, rules)]
+    result = sum([update[int(len(update) / 2)] for update in correct_updates])
     return result
 
 
-# def part2_script(input_file):
-#     return result
+def part2_script(input_file):
+    rules, updates = get_input(input_file)
+    incorrect_updates = [update for update in updates if not update_correct(update, rules)]
+
+    for update in incorrect_updates:
+        swapped = True
+        while swapped:
+            swapped = False
+            for rule in rules:
+                if rule[0] in update and rule[1] in update:
+                    if update.index(rule[0]) > update.index(rule[1]):
+                        update[update.index(rule[0])] = rule[1]
+                        update[update.index(rule[1])] = rule[0]
+                        swapped = True
+                        break
+
+    result = sum([update[int(len(update) / 2)] for update in incorrect_updates])
+    return result
 
 
 if __name__ == '__main__':
@@ -65,5 +72,5 @@ if __name__ == '__main__':
     filename_global = '05_input.txt'
     result1 = part1_script(filename_global)
     print(f'result 1: {result1}')
-    # result2 = part2_script(filename_global)
-    # print(f'result 2: {result2}')
+    result2 = part2_script(filename_global)
+    print(f'result 2: {result2}')
