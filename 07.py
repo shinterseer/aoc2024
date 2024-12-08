a550512471f6e4091a77d9572f42307563e00c06
SImon Hinterseer <hinterseer.simon@gmail.com>
2024-12-08 13:12:17 +0100

    day 07 complete

--- 07.py
+++ 07.py
@@ -3,14 +3,29 @@ import numpy as np
 import time
 
 
-# class TestExample(unittest.TestCase):
-#     filename = '07_testinput.txt'
-#
-#     def test_part1(self):
-#         self.assertEqual(41, part1_script(self.filename))
-#
-#     def test_part2(self):
-#         self.assertEqual(6, part2_script(self.filename))
+class TestExample(unittest.TestCase):
+    filename = '07_testinput.txt'
+
+    def test_part1(self):
+        self.assertEqual(3749, part1_script(self.filename))
+
+    def test_part2(self):
+        self.assertEqual(11387, part2_script(self.filename))
+
+
+def number_to_base(number, base):
+    """
+    from stackoverflow
+    https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
+    """
+    if number == 0:
+        return [0]
+    digits = []
+    while number:
+        digits.append(int(number % base))
+        number //= base
+    return digits[::-1]
+
 
 def get_input(filename):
     with open(filename) as file:
@@ -20,7 +35,6 @@ def get_input(filename):
     test_values = [int(proc[0]) for proc in proc1]
 
     return test_values, equations
-
 
 
 def part1_script(input_file):
@@ -45,27 +59,29 @@ def part1_script(input_file):
                 break
     return result
 
-#
-# def part2_script(input_file):
-#     chars_original, pos = get_input(input_file)
-#     leave_list = list()
-#     loop_list = list()
-#     chars = chars_original.copy()
-#     start_time = time.time()
-#     print(f'total rows: {chars.shape[0]}')
-#     for i in range(chars.shape[0]):
-#         print(f'row: {i}, loops found: {len(loop_list)}', flush=True)
-#         for j in range(chars.shape[1]):
-#             chars = chars_original.copy()
-#             chars[i, j] = '#'
-#             chars_mod, leave, loop = guard_trip(chars.copy(), pos)
-#             if leave:
-#                 leave_list.append((i, j))
-#             if loop:
-#                 loop_list.append((i, j))
-#     print(f'total time for part 2 in s: {time.time() - start_time:.2f}')
-#     result = len(loop_list)
-#     return result
+
+def part2_script(input_file):
+    test_values, equations = get_input(input_file)
+    result = 0
+    for i in range(len(test_values)):
+        num_operations = len(equations[i]) - 1
+        equation = equations[i]
+        test_value = test_values[i]
+        for j in range(3 ** num_operations):
+            j_3 = number_to_base(j, 3)
+            operations = [0] * (num_operations-len(j_3)) + j_3
+            test_result = equation[0]
+            for k in range(len(operations)):
+                if str(operations[k]) == '0':
+                    test_result += equation[k + 1]
+                if str(operations[k]) == '1':
+                    test_result *= equation[k + 1]
+                if str(operations[k]) == '2':
+                    test_result = int(str(test_result) + str(equation[k + 1]))
+            if test_result == test_value:
+                result += test_value
+                break
+    return result
 
 
 if __name__ == '__main__':
@@ -75,6 +91,5 @@ if __name__ == '__main__':
     # filename_global = '07_testinput.txt'
     result1 = part1_script(filename_global)
     print(f'result 1: {result1}')
-    # result2 = part2_script(filename_global)
-    # print(f'result 2: {result2}')15:52
-
+    result2 = part2_script(filename_global)
+    print(f'result 2: {result2}')
